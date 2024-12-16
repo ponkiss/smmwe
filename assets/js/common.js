b7a06fcf26dccbfd38d41b41f3da2f8810aec648
ponkiss <sm719112005@gmail.com>
2024-12-15 18:08:44 -0700

    todo

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,154 +1,85 @@
 "use strict";
 
-var Cmn = function() {};
+var Cmn = {
+    onDownBtn(targetElement, className) {
+        if (targetElement && typeof className === "string") {
+            targetElement.className = className;
+        }
+    },
 
-Cmn.onDownBtn = function(targetElement, className) {
-	if (typeof targetElement === "undefined") {
-		return;
-	}
-	if (typeof className === "string") {
-		targetElement.className = className;
-	}
+    onUpBtn(targetElement, className) {
+        if (targetElement && typeof className === "string") {
+            targetElement.className = className;
+        }
+    },
+
+    setLocation(url) {
+        if (typeof url === "string") {
+            location.href = url;
+        }
+    },
+
+    switch(switches, pages) {
+        const touchClasses = switches.map(() => ({
+            touchstart: "",
+            touchend: "",
+            active: "",
+        }));
+
+        this.setBtnClass = function(touchstartClass, touchendClass, activeClass, switchId, onClick) {
+            const btn = switches[switchId];
+            if (!btn) return;
+
+            touchClasses[switchId] = { touchstart: touchstartClass, touchend: touchendClass, active: activeClass };
+
+            btn.addEventListener("touchstart", () => this.updateClass(btn, touchstartClass));
+            btn.addEventListener("touchend", () => this.updateClass(btn, touchendClass));
+            btn.addEventListener("touchcancel", () => this.updateClass(btn, touchendClass));
+            btn.addEventListener("click", () => this.activatePage(btn, activeClass, onClick));
+            btn.addEventListener("keyup", (event) => {
+                if (event.key === "Enter") this.activatePage(btn, activeClass, onClick);
+            });
+        };
+
+        this.init = function() {
+            switches.forEach((btn, i) => {
+                btn.valid = i === 0;
+                btn.className = i === 0 ? touchClasses[i].active : touchClasses[i].touchend;
+                pages[i][i === 0 ? "show" : "hide"]();
+            });
+        };
+
+        this.activatePage = function(btn, activeClass, onClick) {
+            if (btn.valid) return;
+
+            switches.forEach((sw, i) => {
+                sw.valid = sw === btn;
+                sw.className = sw === btn ? activeClass : touchClasses[i].touchend;
+                pages[i][sw.valid ? "show" : "hide"]();
+            });
+
+            if (typeof onClick === "function") onClick();
+        };
+
+        this.updateClass = function(element, newClass) {
+            if (!element.valid) {
+                element.className = newClass;
+            }
+        };
+    },
+
+    handleModernizrFallback() {
+        if (!Modernizr.svg) {
+            document.querySelectorAll("img").forEach((img) => {
+                img.src = img.src.replace(/\.svg/gi, ".png");
+            });
+        }
+    },
 };
 
-Cmn.onUpBtn = function(targetElement, className) {
-	if (typeof targetElement === "undefined") {
-		return;
-	}
-	if (typeof className === "string") {
-		targetElement.className = className;
-	}
-};
+window.addEventListener("pageshow", () => document.querySelectorAll(".select").forEach((el) => el.classList.remove("select")));
+window.addEventListener("hashchange", () => document.querySelectorAll(".select").forEach((el) => el.classList.remove("select")));
 
-Cmn.setLocation = function(url) {
-	if (typeof url === "string") {
-		location.href = url;
-	}
-};
-
-
-Cmn.switch = function(switches, pages) {
-
-	var touchstart = new Array(switches.length);
-	var touchend = new Array(switches.length);
-	var touchcancel = new Array(switches.length);
-	var active = new Array(switches.length);
-
-	this.setBtnClass = function(touchstart_class, touchend_class, active_class, switch_id, onClick) {
-		_setBtnClass(touchstart_class, touchend_class, active_class, switch_id, onClick);
-	};
-	this.init = function() {
-		for (var i = 0; i < switches.length; i++) {
-			if (i == 0) {
-				switches[i].valid = true;
-				switches[i].className = active[i];
-			} else {
-				switches[i].valid = false;
-				pages[i].hide();
-			}
-		}
-	};
-
-	this.showPage = function(obj) {
-		_showPage(obj);
-	};
-
-	var _showPage = function(obj) {
-		var num = null;
-		for (num = 0; num < switches.length; num++) {
-			if (switches[num] === obj) break;
-		}
-
-		for (var i = 0; i < pages.length; i++) {
-			if (i == num) {
-				_showElementAnimSlide(pages[num]);
-			} else {
-				switches[i].valid = false;
-				switches[i].className = touchend[i];
-				pages[i].hide();
-			}
-		}
-	};
-
-	var _setBtnClass = function(touchstart_class, touchend_class, active_class, switch_id, onClick) {
-		switches[switch_id].addEventListener("touchstart",
-			function() {
-				if (!this.valid) {
-					this.className = touchstart_class;
-				}
-			});
-		switches[switch_id].addEventListener("touchend",
-			function() {
-				if (!this.valid) {
-					this.className = touchend_class;
-				}
-			});
-		switches[switch_id].addEventListener("touchcancel",
-			function() {
-				if (!this.valid) {
-					this.className = touchend_class;
-				}
-			});
-		switches[switch_id].addEventListener("click",
-			function() {
-				if (this.tagName.toLowerCase() == "div") {
-					this.blur();
-				}
-				if (this.valid) {
-					return;
-				}
-				this.className = active_class;
-				this.valid = true;
-				_showPage(this);
-
-				if (typeof onClick === 'function') {
-					onClick();
-				}
-
-			});
-		switches[switch_id].addEventListener("keyup",
-			function() {
-				if (event.keyCode == 13) {
-					if (this.valid) {
-						return;
-					}
-					this.className = active_class;
-					this.valid = true;
-					_showPage(this);
-
-					if (typeof onClick === 'function') {
-						onClick();
-					}
-				}
-			});
-
-		touchstart[switch_id] = touchstart_class;
-		touchend[switch_id] = touchend_class;
-		touchcancel[switch_id] = touchend_class;
-		active[switch_id] = active_class;
-	};
-
-	this.showElementAnimSlide = function(targetElement) {
-		_showElementAnimSlide(targetElement);
-	};
-
-	var _showElementAnimSlide = function(targetElement) {
-		targetElement.show();
-	};
-};
-
-window.addEventListener("pageshow", function() {
-	$(".select").removeClass("select");
-});
-
-window.addEventListener("hashchange", function() {
-	$(".select").removeClass("select");
-});
-
-$(function() {
-	if (!Modernizr.svg) {
-		$('img').each(function() {
-			$(this).attr('src', $(this).attr('src').replace(/\.svg/gi, '.png'));
-		});
-	}
+document.addEventListener("DOMContentLoaded", () => {
+    Cmn.handleModernizrFallback();
 });
